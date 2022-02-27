76b7267aa3dda00dc43832973e394116d58a03d7
dohyun <lhl890@naver.com>
2022-02-28 00:05:50 +0900

    todo list DB 연동 및 crud 개발중

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,48 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import SQLite from './sqlite3';
+
+const sqlite = new SQLite();
+
 const app = express();
 const port = 3000;
+
+function requestTime(req: Request, res: Response, next: NextFunction) {
+  console.log(`request url: ${req.url}`);
+  console.log(`request Time: ${Date.now()}`);
+  next();
+}
+app.use(requestTime);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hello world');
 });
 
-app
-  .route('/todo')
-  .get((req: Request, res: Response) => {
-    res.send('todo list');
-  })
-  .post((req: Request, res: Response) => {
-    res.send('add todo');
-  })
-  .delete((req: Request, res: Response) => {
-    res.send('delete todo');
+app.get('/list', (req: Request, res: Response) => {
+  sqlite.findTodoList(() => {
+    console.log();
   });
+});
+
+app.post('/addTask', (req: Request, res: Response) => {
+  console.log('addTask');
+});
+
+app.post('/removeTask', (req: Request, res: Response) => {
+  console.log('removeTask');
+});
+
+// app
+//   .route('/todo')
+//   .get((req: Request, res: Response) => {
+//     console.log(`todo list`);
+//   })
+//   .post((req: Request, res: Response) => {
+//     res.send('add todo');
+//   })
+//   .delete((req: Request, res: Response) => {
+//     res.send('delete todo');
+//   });
 
 app.listen(port, () => {
-  console.log(`aoo listening on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
